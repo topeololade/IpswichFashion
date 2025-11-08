6ac64160b69be503e4c5a082b6f2b697fe68ae17
s311966 <s311966@uos.ac.uk>
2025-11-08 18:42:52 +0000

    added sample erro trigger for CI/CD

--- ipswitchfashion/urls.py
+++ ipswitchfashion/urls.py
@@ -23,7 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("cart/", include('cart.urls')),
     path("orders/", include("orders.urls")),
+     path("error_sample/", include("error_sample.urls")),
     path('', include('products.urls', namespace='products')),
+    
    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
